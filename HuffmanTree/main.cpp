26d237c05e60375f43174b72fe2ad1398fcc4061
周凯文 <Kevin@zhoukaiwens-MBP.lan>
2014-11-16 00:40:37 +0800

    Initial Commit

---
+++ HuffmanTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HuffmanTree
+//
+//  Created by 周凯文 on 11/16/14.
+//  Copyright (c) 2014 周凯文. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
